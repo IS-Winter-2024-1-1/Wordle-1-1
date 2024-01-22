333a7f98d71ed77967fcc60d199cb94860fa143b
David Bassett <112457273+Yylium@users.noreply.github.com>
2024-01-22 10:19:27 -0700

    some more comments

--- Wordle.py
+++ Wordle.py
@@ -13,13 +13,14 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 def wordle():
 
     def enter_action(s):
+        # Check if word was guessed correctly.
         if s.upper() == random_word:
             row = gw.get_current_row()
             for x in range(5):
                 gw.set_square_color(row, x, "#66BB66")
             gw.show_message('You guessed it!')
 
-
+        # Check if guessed word is in dictionary.
         elif s.lower() in FIVE_LETTER_WORDS:
             row = gw.get_current_row()
 
@@ -30,35 +31,35 @@ def wordle():
                 guessedWord.append(s[x])
                 tempRandom.append(random_word[x])
 
-
+            # Check for correct positions.
             for x in range(5):
                 if guessedWord[x] == random_word[x]:
                     gw.set_square_color(row, x, "#66BB66")
 
-                    # Replacing the correct letter in each temp string with symbols so it will be excluded from future
+                    # Replace the correct letter in each temp string with symbols so it will be excluded from future
                     # processing.
                     guessedWord[x] = '_'
                     tempRandom[x] = '*'
 
-
+            # Check for correct word wrong position.
             for x in range(5):
                 if (guessedWord[x] in tempRandom):
                     gw.set_square_color(row, x, "#CCBB66")
 
-                    # Replacing the found letter in the random word so it will be excluded from future processing
+                    # Replace the found letter in the random word so it will be excluded from future processing.
                     tempRandom[tempRandom.index(guessedWord[x])] = '*'
 
+                # Color everything else gray.
                 elif gw.get_square_color(row, x) == "#FFFFFF":
-                    print(gw.get_square_color(row, x))
                     gw.set_square_color(row, x, "#999999")
 
 
 
-
+            # Check if it's game over. If not, ove to the next row.
             if row + 1 < N_ROWS:
                 gw.show_message('') # Blank out message
-
                 gw.set_current_row(gw.get_current_row() + 1)
+
             else:
                 gw.show_message('Game Over')
 
