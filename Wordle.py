e7f3259e585aeb7217dea3d1f12da424f985f93f
David Bassett <112457273+Yylium@users.noreply.github.com>
2024-01-22 10:11:44 -0700

    ready for testing

--- Wordle.py
+++ Wordle.py
@@ -13,9 +13,51 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 def wordle():
 
     def enter_action(s):
-        if s.lower() in FIVE_LETTER_WORDS:
-            if gw.get_current_row() + 1 < N_ROWS:
-                gw.show_message('That\'s a word, yeah')
+        if s.upper() == random_word:
+            row = gw.get_current_row()
+            for x in range(5):
+                gw.set_square_color(row, x, "#66BB66")
+            gw.show_message('You guessed it!')
+
+
+        elif s.lower() in FIVE_LETTER_WORDS:
+            row = gw.get_current_row()
+
+            # These variables are here so we can edit them for processing without changing the original words.
+            guessedWord = []
+            tempRandom = []
+            for x in range(5):
+                guessedWord.append(s[x])
+                tempRandom.append(random_word[x])
+
+
+            for x in range(5):
+                if guessedWord[x] == random_word[x]:
+                    gw.set_square_color(row, x, "#66BB66")
+
+                    # Replacing the correct letter in each temp string with symbols so it will be excluded from future
+                    # processing.
+                    guessedWord[x] = '_'
+                    tempRandom[x] = '*'
+
+
+            for x in range(5):
+                if (guessedWord[x] in tempRandom):
+                    gw.set_square_color(row, x, "#CCBB66")
+
+                    # Replacing the found letter in the random word so it will be excluded from future processing
+                    tempRandom[tempRandom.index(guessedWord[x])] = '*'
+
+                elif gw.get_square_color(row, x) == "#FFFFFF":
+                    print(gw.get_square_color(row, x))
+                    gw.set_square_color(row, x, "#999999")
+
+
+
+
+            if row + 1 < N_ROWS:
+                gw.show_message('') # Blank out message
+
                 gw.set_current_row(gw.get_current_row() + 1)
             else:
                 gw.show_message('Game Over')
@@ -25,23 +67,24 @@ def wordle():
 
 
 
+
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
     random_word = random.choice(FIVE_LETTER_WORDS).upper()
 
+    print(random_word)
 
 
     # For milestone 1. Commented out for future purposes.
     '''
+
     # print(f"Random Word: {random_word}")
     for column, letter in enumerate(random_word):
         # print(f"Setting letter '{letter}' in row 0, column {column}")
         if column < N_COLS:
             gw.set_square_letter(0, column, letter)
     '''
-
-
 
 # Startup code
 
