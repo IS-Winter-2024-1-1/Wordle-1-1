6a6ba556aa581ed524654269c20e18ff22d81592
David Bassett <112457273+Yylium@users.noreply.github.com>
2024-01-29 12:55:02 -0700

    IT'S ALIVE

--- main.py
+++ main.py
@@ -5,34 +5,40 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from swahiliDictionary import SWAHILI_FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
-def colorblindY(wordBank):
+
+
+def colorblindY():
+    global colorblind, wordBank
     colorblind = True
     root.destroy()
     
-    wordle(wordBank, colorblind)
+    wordle(wordBank)
 
-def colorblindN(wordBank):
+def colorblindN():
+    global colorblind, wordBank
     colorblind = False
     root.destroy()
 
-    wordle(wordBank, colorblind)
+    wordle(wordBank)
 
 def english():
+    global wordBank
     wordBank = FIVE_LETTER_WORDS
-    root.destroy()
+    p = tk.Toplevel(root)
+
 
     # Ask for colorblindness
-    label = tk.Label(root, text='ColorBlind Mode')
+    label = tk.Label(p, text='\tEnable ColorBlind Mode?   \t\n')
 
-    bYes = tk.Button(root, text="Yes", command=colorblindY)
-    bNo = tk.Button(root, text="No", command=colorblindN)
+    bYes = tk.Button(p, text="Yes", command=colorblindY)
+    bNo = tk.Button(p, text="No", command=colorblindN)
     
     for el in [label, bYes, bNo]:
         el.pack()
 
 
 def swahili():
-    # Assuming you have FIVE_LETTER_WORDS_SWAHILI defined in your WordleDictionary module
+    global wordBank
     wordBank = SWAHILI_FIVE_LETTER_WORDS
     root.destroy()
     # Ask for colorblindness
@@ -53,11 +59,24 @@ def modal(question):
 
 def wordle(wordBank):
     def enter_action(s):
+        # Colors
+        global colorblind
+        if not colorblind:
+            correct = "#66BB66"
+            close = "#CCBB66"
+            wrong = "#999999"
+
+        else:
+            correct = "#0C7BDC"
+            close = "#FFC20A"
+            wrong = "#999999"
+
+
          # Check if word was guessed correctly.
         if s.upper() == random_word:
             row = gw.get_current_row()
             for x in range(5):
-                gw.set_square_color(row, x, "#66BB66")
+                gw.set_square_color(row, x, correct)
             gw.show_message('You guessed it!')
 
         # Check if guessed word is in dictionary.
@@ -74,24 +93,24 @@ def wordle(wordBank):
             # Check for correct positions.
             for x in range(5):
                 if guessedWord[x] == random_word[x]:
-                    gw.set_square_color(row, x, "#66BB66")
+                    gw.set_square_color(row, x, correct)
 
                     # Replace the correct letter in each temp string with symbols so it will be excluded from future
                     # processing.
                     guessedWord[x] = '_'
                     tempRandom[x] = '*'
 
-            # Check for correct word wrong position.
+            # Check for correct letter wrong position.
             for x in range(5):
                 if (guessedWord[x] in tempRandom):
-                    gw.set_square_color(row, x, "#CCBB66")
+                    gw.set_square_color(row, x, close)
 
                     # Replace the found letter in the random word so it will be excluded from future processing.
                     tempRandom[tempRandom.index(guessedWord[x])] = '*'
 
                 # Color everything else gray.
                 elif gw.get_square_color(row, x) == "#FFFFFF":
-                    gw.set_square_color(row, x, "#999999")
+                    gw.set_square_color(row, x, wrong)
 
 
 
@@ -115,5 +134,5 @@ def wordle(wordBank):
 if __name__ == "__main__":
     root = tk.Tk()
     questionsAsked = []
-    modal("What language would you like to play in?")
+    modal("What language would you like to play in?\n")
     root.mainloop()
